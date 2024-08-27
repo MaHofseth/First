ba9293ced840503e4d1cf1bf4bce657a7eb807e2
MaHofseth <mahofseth@protonmail.com>
2024-08-27 13:24:48 +0200

    aeq

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+String navn,adresse,telefonnummer;
+navn="Ole Olsen";
+adresse="Askerveien 82";
+telefonnummer="555-555-5555";
+String ut=navn+" "+adresse+" "+telefonnummer;
+        System.out.println(ut);
+    }
+}
